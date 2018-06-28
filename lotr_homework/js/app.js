a528894f2b71b00cd417d156c5b18f6bbf39a1fb
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-06-28 17:59:25 -0600

    chapter 12 complete

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -339,14 +339,18 @@ itsDangerousToGoAlone();
 // Chapter 12
 // ============
 const weWantsIt = () => {
-
+  $("#Mordor").append("<div id='gollum'></div>");
+  $("#the-ring").remove().appendTo("#gollum");
+  $("#gollum").remove().appendTo("#mount-doom");
+ }; 
   // 1. Create a div with an id of 'gollum' and add it to Mordor
 
   // 2. Move the ring from Frodo and give it to Gollum
 
   // 3. Move Gollum into Mount Doom
 
-};
+
+weWantsIt();
 
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 12 complete - Gollum is trying to get the ring".
